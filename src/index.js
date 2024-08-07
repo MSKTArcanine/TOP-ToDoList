d8799e779e8334203a7077182793595753ccef0d
MSKTArcanine <marcantoine1999@live.fr>
2024-08-07 03:29:11 +0200

    Added instructions for next time" git status

---
+++ src/index.js
@@ -0,0 +1,28 @@
+/*
+La logique : 
+On fait une classe pour chaque projects obj.
+On balance chaque method en tant qu'instance method, osef de la static (moins chiant avec this)
+On JSON stringify pour save
+On JSON parse puis on rebalance les trucs dans la classe pour ravoir les methods
+voila.
+
+DOM : 
+On fera un object par défaut si on a rien dans le localStorage.
+=> Deja ouvert si possible.
+=> On check ça avec onLoad si possible.
+
+Ensuite on a juste a display en fonction du project ouvert.
+=> Faire une variable "ACTIVE PROJECT" pour etre sur de bien prendre le bon project dans l'array d'objets.
+=> Bien faire gaffe a set un MAX a 12.
+
+On fait les modifs 'temps réel' avec la form validation.
+=> Pas oublier de faire un pti pop up tout con.
+
+Ensuite onSave => On stringify le bordel.
+onSaveAndQuit => On stringify et on efface le bordel au milieu.
+onQuitNoSave => On efface simplement.
+onDelete => On delete directement depuis le localStorage l'index de l'array
+               => A voir si on peut MAP les arrays pour que la value de "index" soit automatique.
+               => Sinon osef, on set la taille à 12 et on fait des trous dans l'array, tfasson c'est pas si grave.
+Bref voila. Pas trop compliqué en somme. LOL.
+*/
